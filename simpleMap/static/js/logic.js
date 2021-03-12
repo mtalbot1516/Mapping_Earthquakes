34c0fb44c14b22c3b205212f7dabc3405b6003b7
Matthew Talbot <mtalbot1516@gmail.com>
2021-03-12 16:45:28 -0600

    second push

--- simpleMap/static/js/logic.js
+++ simpleMap/static/js/logic.js
@@ -1,6 +1,6 @@
 console.log("working")
 // Create the map object with a center and zoom level
-let map = L.map('mapid').setView([40.7, -94.5], 4)
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
