a96f006de80a7e941ee929865e66d0561ee34168
Matthew Talbot <mtalbot1516@gmail.com>
2021-03-12 19:58:29 -0600

    repo

--- mappingSingleLine/static/js/logic.js
+++ mappingSingleLine/static/js/logic.js
@@ -1,6 +1,6 @@
 console.log("working")
 // Create the map object with a center and zoom level
-let map = L.map('mapid').setView([36.1733, -120.1794], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -11,17 +11,33 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Add a marker to the map for LA
 // Coordinates for each point to be used in the line.
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-    ];
-    
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 // Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "red"
-}).addTo(map)
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+        console.log(feature);
+        return L.marker(latlng)
+        .bindPopup[("<h2>" + feature.properties.city + "</h2>")]
+    }
+    }).addTo(map)
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
